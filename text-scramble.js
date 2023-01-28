8a60d1ec5f95871bb32b2ce7f09503a88ecb04d0
chinh4thepro <chinh4thepro@gmail.com>
2023-01-28 18:02:48 -0500

    aaaaa

---
+++ text-scramble.js
@@ -0,0 +1,20 @@
+const lettersandnumbers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+document.querySelector("li").onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if(index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+                return lettersandnumbers[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+    
+        if(iterations >= event.target.dataset.value.length) clearInterval(interval);    
+        
+        iterations += 1 / 3;
+    }, 30);
+}
