c61506bcfad1712903141a61960b5506ff32c489
chinh4thepro <chinh4thepro@gmail.com>
2024-05-10 21:46:33 -0400

    errrr

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'personal': ['Roboto', '"Noto Emoji"'],
+    },
+    extend: {
+      'animation': {
+        'gradient-move-xy': 'gradient-move-xy 10s ease infinite',
+      },
+      'keyframes': {
+        'gradient-move-xy': {
+          '0%, 100%': {
+            'background-size':'400% 400%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size':'200% 200%',
+            'background-position': 'right center'
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
